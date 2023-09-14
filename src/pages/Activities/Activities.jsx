cbefda54b5d50b60ff1180fa4f6b479dd7f09261
onlyaditya <onlyaditya001@gmail.com>
2023-09-15 00:58:03 +0530

    restructure

---
+++ src/pages/Activities/Activities.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { SideBar } from "../../components/SideBar/SideBar";
+
+export const Activities = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <SideBar />
+        <div />
+      </div>
+    </div>
+  );
+};
