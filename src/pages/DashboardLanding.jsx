d52aab4fb1d25656d0c717199c0c17fc6046aa9b
Sunil Chaudhary <sunilchaudhary7789@gmail.com>
2023-09-14 14:35:12 +0530

    seprated all svg images in assets folder

--- src/pages/DashboardLanding.jsx
+++ src/pages/DashboardLanding.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import Sidebar from '../components/Sidebar/Sidebar'
 
 const DashboardLanding = () => {
   return (
     <div>
       <Navbar/>
+      <Sidebar/>
     </div>
   )
 }
