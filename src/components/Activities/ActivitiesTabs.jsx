7fc63cd3840814419be5b04438715591c70eb76b
onlyaditya <onlyaditya001@gmail.com>
2023-09-19 19:08:35 +0530

    added activity category

--- src/components/Activities/ActivitiesTabs.jsx
+++ src/components/Activities/ActivitiesTabs.jsx
@@ -1,12 +1,27 @@
 import { Box, Image } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import liveImage from "../../assets/images/activities-live.png";
 import masterclassImage from "../../assets/images/activities-masterclass.png";
 import contestImage from "../../assets/images/activities-contests.png";
 import eventsImage from "../../assets/images/activities-events.png";
 import { ActivityItem } from "./ActivityItem";
+import { useDispatch } from "react-redux";
+import { getActivities } from "../../redux/activityReducer/action";
 
 export const ActivitiesTabs = () => {
+  const [type, setType] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const paramObj = {
+      params: type !== null && {
+        type,
+      },
+    };
+
+    dispatch(getActivities(paramObj));
+  }, [type]);
+
   return (
     <Box display={"flex"} gap={"5"} my={"33"} mx={"33"} alignItems={"center"}>
       <Image src={liveImage} w={"fit-content"} width={"76px"} height={"76px"} />
@@ -15,18 +30,21 @@ export const ActivitiesTabs = () => {
         text={"Masterclass"}
         bg1={"#FFFFFF"}
         bg2={"#FFE4E4"}
+        changeType={() => setType(1)}
       />
       <ActivityItem
         image={eventsImage}
         text={"Events"}
         bg1={"#FFFFFF"}
         bg2={"#FFF8E4"}
+        changeType={() => setType(null)}
       />
       <ActivityItem
         image={contestImage}
-        text={"Contests"}
+        text={"Webinars"}
         bg1={"#FFFFFF "}
         bg2={"#E4FFFA"}
+        changeType={() => setType(0)}
       />
     </Box>
   );
--- src/redux/activityReducer/action.js
+++ src/redux/activityReducer/action.js
@@ -5,11 +5,13 @@ import {
   GET_ACTIVITIES_SUCCESS,
 } from "../actionTypes";
 
-export const getActivities = (dispatch) => {
+export const getActivities = (params) => (dispatch) => {
   dispatch({ type: GET_ACTIVITIES_REQUEST });
   axios
-    .get("https://dash-board.up.railway.app/activities")
+    .get("https://dash-board.up.railway.app/activities", params && params)
     .then((res) => {
+      console.log(params);
+      console.log(res.data);
       dispatch({ type: GET_ACTIVITIES_SUCCESS, payload: res.data });
     })
     .catch((err) => {
